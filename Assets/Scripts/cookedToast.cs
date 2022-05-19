0471f5690a2a47746c2b538ea661b9e491fcd6f3
toaddoesnot <laiskusn7@gmail.com>
2022-05-18 23:38:24 -0400

    Prefabs Updated
    Stove mechanics is improved

--- Assets/Scripts/cookedToast.cs
+++ Assets/Scripts/cookedToast.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 
 public class cookedToast : MonoBehaviour
 {
-    
+    public Inventory inventory;
+    public FoodClasses buttons;
+
+    public GameObject inventoryManager;
+    public GameObject self;
+
+    private void Start()
+    {
+        inventoryManager = GameObject.FindGameObjectWithTag("Inventory");
+        inventory = inventoryManager.GetComponent<Inventory>();
+        buttons = inventoryManager.GetComponent<FoodClasses>();
+    }
 
     void OnMouseDown()
     {
-        
+        buttons.currentFoods = -1;
+        if (inventory.sthCooked)
+        {
+            
+        }
+        else
+        {
+            inventory.ToastCooked = true;
+            Object.Destroy(self);
+        }
     }
 }
